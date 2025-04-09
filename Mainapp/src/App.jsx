5b31628602e22d13c877bf91892002e434fa1373
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-04-09 23:17:46 +0530

    adding react-router-dom for routing and creating some routes

--- Mainapp/src/App.jsx
+++ Mainapp/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import WikipediaSearch from './components/WikipediaSearch';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/Newspage';
 
 function App() {
   return (
-    <div className="App">
-      <WikipediaSearch />
+    // <div className="App">
+    //   <WikipediaSearch />
+    // </div>
+    <div className=''>
+      <Router>
+        <Routes>
+          <Route path="/search" element={<WikipediaSearch />} />
+          <Route path="/news" element={<NewsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
